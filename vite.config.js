d1b7b29b91e0c2536435cfbde866ca406fd52a5a
zayhentai1 <113987905+zayhentai1@users.noreply.github.com>
2023-06-11 10:01:41 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -21,4 +21,10 @@ export default defineConfig({
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
 		},
 	},
+	base:'./',
+	build:{
+		commonjsOptions:{
+			transformMixedEsModules:true,
+		},
+	}
 })
